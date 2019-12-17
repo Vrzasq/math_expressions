990ccd090fefb3763230d551b960b35d65a596e1
Jakub Romel <ridzio1986@hotmail.com>
2019-12-17 22:01:41 +0100

    add factory tests

---
+++ math_expressions/math_expressions.Test/ExpressionFactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using math_expressions.ExpressionSolver.MathSolvers;
+using math_expressions.ExpressionSolver;
+using math_expressions.ExpressionSolver.Factory;
+
+namespace math_expressions.Test
+{
+    [TestClass]
+    public class ExpressionFactoryTest
+    {
+        [TestMethod]
+        public void TestCSharpMathSolverCreation()
+        {
+            IMathExpressionSolver solver = ExpressionSolverFactory.GetSolver(Solver.CSharpMathSolver);
+
+            Assert.IsInstanceOfType(solver, typeof(CSharpMathSolver));
+        }
+
+        [TestMethod]
+        public void TestExpressionMathSolverCreation()
+        {
+            IMathExpressionSolver solver = ExpressionSolverFactory.GetSolver(Solver.ExpressionMathSolver);
+
+            Assert.IsInstanceOfType(solver, typeof(ExpressionMathSolver));
+        }
+    }
+}
